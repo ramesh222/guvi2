a2abe57dd0c0766807cfe324fb77d766bc577f0b
Ramesh <rameshatdgl@gmail.com>
2016-09-08 18:11:21 +0530

    Create word.c

---
+++ word.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+char a[100];
+printf("enter the string");
+gets(a);
+int count;
+for(int i=0;a[1]!='\t';i++)
+{
+count ++;
+}
+printf("total no of words %d",count);
+return 0;
+}
