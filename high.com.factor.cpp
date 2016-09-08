c2b9fd1555f77575c8db66d244d7a2dc379aa29a
Ramesh <rameshatdgl@gmail.com>
2016-09-08 17:04:05 +0530

    Update high.com.factor.cpp

--- high.com.factor.cpp
+++ high.com.factor.cpp
@@ -6,8 +6,8 @@ void main() {
     cin >> n1 >> n2;
       while(n1 != n2) {
         if(n1 > n2)
-            n1 -= n2;
+            n1=n1-n2;
         else
-            n2 -= n1;
+            n2=n2-n1;
     }
     cout << "HCF = " << n1;}
