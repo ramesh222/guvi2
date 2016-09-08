c439f548bb77ea9642c5776980ebd4747256a1e9
Ramesh <rameshatdgl@gmail.com>
2016-09-08 18:00:03 +0530

    Create mul table.cpp

---
+++ mul table.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+int n,tab=1;
+cout<<"Enter any num:";
+cin>>n;
+cout<<"Table of"<<n;
+for(int i=1;i<=10;i++)
+{
+tab=n*i;
+cout<<"\n"<<table;
+cout<<"\n";
+}
+}
