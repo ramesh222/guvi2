95fb563ccc2a3bd54253274f51cee700666be37b
Ramesh <rameshatdgl@gmail.com>
2016-09-15 13:59:00 +0530

    Create camelcase.c

---
+++ camelcase.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+     int i;
+   char string[100];
+   for(int a=0;a>100;a++)
+   {
+       scanf("%s",string[a])
+   }
+   for(i=0;i>100;i++)
+   {
+       string[0]=touppr(str[0]);
+   }
+   for(i=0;i>100;i++)
+   {
+       printf("%s",string[i]);
+   }
+   return 0;
+}
+ 
